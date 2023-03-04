50a9940e1a923a88fee8fc8e7a262cff4c4f8c8a
Craig McFarlane <craig@delaneymorgan.com.au>
2023-03-04 11:36:40 +1100

    - package builder runs without errors

--- deployment/PackageBuilder.py
+++ deployment/PackageBuilder.py
@@ -3,6 +3,21 @@ import os
 import shutil
 
 
+class PackagingError(Exception):
+    def __init__(self, command, error):
+        self.command = command
+        self.error = error
+        super().__init__(f"Packaging error - Command: {self.command}: Error: {self.error}")
+        return
+
+class VersionInsertionError(Exception):
+    def __init__(self, command, error):
+        self.command = command
+        self.error = error
+        super().__init__(f"Version insertion error - Command: {self.command}: Error: {self.error}")
+        return
+
+
 class PackageBuilder:
     def __init__(self, package_name, build__info_filepath, version):
         with open(build__info_filepath, "r") as json_file:
@@ -22,8 +37,10 @@ class PackageBuilder:
         template_folder = os.path.join(current_folder, f"deb_{self.package_name}")
         shutil.copytree(template_folder, deb_folder)
         control_file = os.path.join(deb_folder, "DEBIAN/control")
-        version_cmd = r'sed -i s/\<version>\>/{}/g {}'.format(self.version, control_file)
-        os.system(version_cmd)
+        version_cmd = r"sed -i 's_<version>_{}_g' {}".format(self.version, control_file)
+        error = os.system(version_cmd)
+        if error:
+            raise VersionInsertionError(version_cmd, error)
         src_folder = os.path.abspath(os.path.join(current_folder, f'../{self.package_name}'))
         deployment_dir = self.build_info["install_prefix_path"][1:]
         lts_target_folder = os.path.join(deb_folder, deployment_dir)
@@ -32,11 +49,13 @@ class PackageBuilder:
             src_file = os.path.abspath(os.path.join(src_folder, this_file))
             dest_file = os.path.abspath(os.path.join(lts_target_folder, this_file))
             dest_folder = os.path.dirname(dest_file)
-            if not os.path.exists(dest_file):
+            if not os.path.exists(dest_folder):
                 os.makedirs(dest_folder)
             shutil.copy(src_file, dest_file)
 
         pkg_cmd = f"dpkg -b {deb_folder} {self.package_name}-{self.version}.deb"
-        os.system(pkg_cmd)
+        error = os.system(pkg_cmd)
+        if error:
+            raise PackagingError(pkg_cmd, error)
         shutil.rmtree(deb_folder, ignore_errors=True)
         return
--- deployment/build_deb_packages.py
+++ deployment/build_deb_packages.py
@@ -7,7 +7,25 @@ import PackageBuilder
 # =============================================================================
 
 
-BUILD_INFO_FILENAME = "LocalTimeServer_file_list.json"
+BUILD_INFO_FILENAME = "../LocalTimeServer_file_list.json"
+
+
+# =============================================================================
+
+
+class ValidateVersionNumber(argparse.Action):
+    def __call__(self, parser, namespace, values, option_string=None):
+        parts = values.split(".")
+        if len(parts) != 3:
+            parser.error(f"Please enter a valid version number in <major>.<minor>.<maintenance> format")
+        for part in parts:
+            if not part.isdigit():
+                parser.error(f"Please enter a valid version number in <major>.<minor>.<maintenance> format")
+        setattr(namespace, self.dest, values)
+        return
+
+
+# =============================================================================
 
 
 def arg_parser():
@@ -18,7 +36,7 @@ def arg_parser():
     """
     parser = argparse.ArgumentParser(description="makes LocalTimeServer a package for the specified target",
                                      add_help=False)
-    parser.add_argument("-v", "--version", type=int, help="version#, i.e. x.y.z", required=True)
+    parser.add_argument("-v", "--version", type=str, help="version#, i.e. x.y.z", required=True, action=ValidateVersionNumber)
     parser.add_argument("-?", "--help", help="show help message and quit", action="help")
     args = parser.parse_args()
     return args
